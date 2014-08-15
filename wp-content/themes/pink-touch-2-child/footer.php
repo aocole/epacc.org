87d4c034203f85f17da13fc6a32bce91bb7c466e
Andrew Cole <aocole@gmail.com>
2014-08-15 14:27:58 -0700

    Initial commit

---
+++ wp-content/themes/pink-touch-2-child/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Pink Touch 2
+ */
+
+/* A sidebar in the footer? Yep. You can can customize
+ * your footer with three columns of widgets.
+ */
+get_sidebar( 'footer' );
+?>
+
+		</div><!-- /#content -->
+	</div><!-- /#wrapper -->
+
+	<div id="footer">
+		<div class="wrapper">
+			<p class="info-theme" role="contentinfo">Header image adapted from: <a href="https://www.flickr.com/photos/brke/4731073369" title="justin vs the wilderness part 1 by Brooke Graham, on Flickr">justin vs the wilderness part 1 by Brooke Graham</a></p>
+		</div>
+	</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
